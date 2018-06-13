00f84819767ad496a09bbf6aa400090689728d88
Richie Zhang <richiezhang256@gmail.com>
2018-06-13 01:29:33 -0700

    Check for about page visibility in a try and except block.

--- src/apps/about/views.py
+++ src/apps/about/views.py
@@ -3,7 +3,7 @@ from apps.utils.functions import parse_formatting
 
 # Create your views here.
 
-from .models import About
+from .models import About, ThirdPartyLicenses
 
 def about_abstract(request, model, template, navbar_selected=False):
     '''Abstract function for the pages'''
@@ -13,11 +13,12 @@ def about_abstract(request, model, template, navbar_selected=False):
         content.content = parse_formatting(content.content,
                                            html=content.html,
                                            markdown=content.markdown)
-    except model.DoesNotExist:
-        content = None
 
-    # Only display the page if it has been set to 'visible'.
-    if not content.visible:
+        # Only display the page if it has been set to 'visible'.
+        if not content.visible:
+            content = None
+            
+    except model.DoesNotExist:
         content = None
 
     context = {
@@ -32,3 +33,9 @@ def about(request):
     '''About page'''
 
     return about_abstract(request, About, 'about.html', True)
+
+def third_party_licenses(request):
+    '''Third party licenses page'''
+
+    return about_abstract(request, ThirdPartyLicenses, 
+                          'third_party_licenses.html', False)
