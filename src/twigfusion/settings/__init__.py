1d9631036e581c4effed0435d292e2ad120d54f6
Richie Zhang <richiezhang256@gmail.com>
2018-07-01 00:39:32 -0700

    Implement all settings in the XML configuration files.

--- src/twigfusion/settings/__init__.py
+++ src/twigfusion/settings/__init__.py
@@ -1,6 +1,7 @@
 import defusedxml.ElementTree as xml
 import os.path
 from apps.utils.functions import str_to_bool, get_unique_xml_element
+from apps.utils.exceptions import XmlElementNotFound
 from .base import *
 
 class ConfigurationNotFound(Exception):
@@ -102,8 +103,161 @@ if not SECRET_KEY:
         'file consists of an empty string.'
     )
 
+# Apply database configurations if applicable.
+database = get_unique_xml_element(mode, 'database')
+database_keys = [
+    # The accepted database configuration data.
+    'engine',
+    'name',
+    'user',
+    'password',
+    'host',
+    'port',
+]
+for key in database_keys:
+    try:
+        DATABASES['default'][key] = get_unique_xml_element(database, key).text
+    except XmlElementNotFound:
+        pass
+
+# Applies Google services configuration data if applicable
+try:
+    google = get_unique_xml_element(mode, 'google')
+
+    try:
+        GOOGLE_SITE_VERIFICATION = get_unique_xml_element(google, 'site-verification').text
+    except XmlElementNotFound:
+        
+    # TODO: Implement reCAPTCHA support.
+    try:
+        recaptcha = get_unique_xml_element(google, 'recaptcha')
+
+        try:
+            recaptcha_enabled = str_to_bool(recaptcha.get('enabled'))
+        except XmlElementNotFound:
+            recaptcha_enabled = False
+
+        if recaptcha_enabled:
+            try:
+                RECAPTCHA_PUBLIC_KEY = get_unique_xml_element(recaptcha, 'public-key').text
+            except XmlElementNotFound:
+                raise ConfigurationError('The reCAPTCHA public key is required when reCAPTCHA is enabled.')
+                        try:
+                RECAPTCHA_PRIVATE_KEY = get_unique_xml_element(recaptcha, 'private-key').text
+            except XmlElementNotFound:
+                raise ConfigurationError('The reCAPTCHA private key is required when reCAPTCHA is enabled.')
+
+        del recaptcha
+
+    except XmlElementNotFound:
+        pass
+
+    del google
+
+except XmlElementNotFound:
+    pass
+
+# Applies analytics configurations if applicable.
+try:
+    analytics = get_unique_xml_element(mode, 'analytics')
+
+    try:
+        ANALYTICAL_INTERNAL_IPS = []
+
+        for ip in get_unique_xml_element(analytics, 'internal-ips'):
+            # Iterate through the list of IP addresses.
+            if ip.tag = 'address':
+                # Only use IP addresses in the <address> tags.
+                ANALYTICAL_INTERNAL_IPS.append(ip)
+
+        del analytics
+    except XmlElementNotFound:
+        pass
+    try:
+        ANALYTICAL_AUTO_IDENTIFY = str_to_bool(
+            get_unique_xml_element(analytics, 'auto-identify').get('enabled')
+        )
+    except XmlElementNotFound:
+        pass
+
+    # Applies Google Analytics configurations if applicable
+    try:
+        google_analytics = get_unique_xml_element(analytics, 'google')
+
+        if str_to_bool(google_analytics.get('enabled')):
+            try:
+                GOOGLE_ANALYTICS_PROPERTY_ID = get_unique_xml_element(google_analytics, 'id').text
+            except XmlElementNotFound:
+                pass
+            try:
+                GOOGLE_ANALYTICS_SITE_SPEED = get_unique_xml_element(google_analytics, 'sample-rate').text
+            except XmlElementNotFound:
+                pass
+            try:
+                GOOGLE_ANALYTICS_DISPLAY_ADVERTISING = str_to_bool(
+                    get_unique_xml_element(google_analytics, 'display-advertising').get('enabled')
+                )
+            except XmlElementNotFound:
+                pass
+            try:
+                GOOGLE_ANALYTICS_SITE_SPEED = str_to_bool(
+                    get_unique_xml_element(google_analytics, 'site-speed').get('enabled')
+                )
+            except XmlElementNotFound:
+                pass
+            try:
+                GOOGLE_ANALYTICS_SITE_SPEED_SAMPLE_RATE = get_unique_xml_element(google_analytics, 'site-speed').get('sample-rate')
+            except XmlElementNotFound:
+                pass
+            try:
+                GOOGLE_ANALYTICS_ANONYMIZE_IP = str_to_bool(
+                    get_unique_xml_element(google_analytics, 'anonymize-ip').get('enabled')
+                )
+            except XmlElementNotFound:
+                pass
+            try:
+                GOOGLE_ANALYTICS_SESSION_COOKIE_TIMEOUT = get_unique_xml_element(google_analytics, 'cookie-timeout').get('session')
+            except XmlElementNotFound:
+                pass
+            try:
+                GOOGLE_ANALYTICS_VISITOR_COOKIE_TIMEOUT = get_unique_xml_element(google_analytics, 'cookie-timeout').get('visitor')
+            except XmlElementNotFound:
+                pass
+            except
+
+        del google_analytics
+
+    except XmlElementNotFound:
+        pass
+
+    # Applies Matomo (Piwik) configurations if applicable
+    try:
+        matomo = get_unique_xml_element(analytics, 'matomo')
+
+        if str_to_bool(matomo.get('enabled')):
+            try:
+                PIWIK_SITE_ID  = get_unique_xml_element(matomo, 'id').text
+            except XmlElementNotFound:
+                pass
+            try:
+                PIWIK_DOMAIN_PATH  = get_unique_xml_element(matomo, 'server').text
+            except XmlElementNotFound:
+                pass
+
+        del matomo
+
+    except XmlElementNotFound:
+        pass
+
+del analytics
+
+except XmlElementNotFound:
+    pass
+
 # Delete non-settings variables
 del config_file
 del config
 del mode_name
 del mode
+del database
+del database_keys
