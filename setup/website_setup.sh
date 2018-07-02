fee5daf61ab7340f87274ef1a15881011b9008cd
Richie Zhang <richiezhang256@gmail.com>
2018-07-01 23:16:58 -0700

    Add a script for setting up the website on a server.

---
+++ setup/website_setup.sh
@@ -0,0 +1,16 @@
+## TwigFusion Website Setup
+
+# Install website dependencies
+pip install -r requirements.txt
+npm install
+
+# Go to the source folder
+cd src
+
+# Build static files
+tsc
+python3 manage.py collectstatic
+
+# Migrate database
+python3 manage.py makemigrations
+python3 manage.py migrate
