b4de37055a2513a7b301c2ee4e0510e175c49d61
Richie Zhang <richiezhang256@gmail.com>
2019-03-21 14:32:52 -0700

    Create website_setup.cmd.

---
+++ setup/website_setup.cmd
@@ -0,0 +1,21 @@
+:: Website Setup
+
+@echo off
+
+:: Go to the project root
+cd ..
+
+:: Install website dependencies
+pip install -r requirements.txt
+npm install
+
+:: Go to the source folder
+cd src
+
+:: Build static files
+tsc
+python3 manage.py collectstatic
+
+:: Migrate database
+python3 manage.py makemigrations
+python3 manage.py migrate
