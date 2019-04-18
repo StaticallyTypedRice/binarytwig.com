a1605534994db0c353baeccb0853bf4f5c1c2371
Richie Zhang <richiezhang256@gmail.com>
2019-04-17 19:12:30 -0700

    Create a database setup script.

---
+++ setup/setup.sql
@@ -0,0 +1,6 @@
+-- Use these queries to set up the database for MySQL or MariaDB
+
+CREATE DATABASE binarytwig CHARACTER SET UTF8;
+CREATE USER binarytwig@localhost IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON binarytwig.* TO binarytwig@localhost;
+FLUSH PRIVILEGES;
